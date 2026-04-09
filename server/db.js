f41c8451696a25a8091a5a55035943841b99eb1f
IanNarito <iannarito116@gmail.com>
2026-04-10 05:36:50 +0800

    Update db.js for Aiven Port and SSL

--- server/db.js
+++ server/db.js
@@ -3,11 +3,14 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const pool = mysql.createPool({
-  // It will try to use the .env value, otherwise it uses the XAMPP default
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',      
   password: process.env.DB_PASS || '',      
   database: process.env.DB_NAME || 'careerflow_db',
+  port: process.env.DB_PORT || 3306, // <--- ADDED: Tells the app to use Aiven's custom port
+  ssl: {
+      rejectUnauthorized: false      // <--- ADDED: Required for Aiven's secure cloud connection
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -18,7 +21,7 @@ pool.getConnection((err, connection) => {
   if (err) {
     console.error("❌ MySQL Connection Failed: ", err.message);
   } else {
-    console.log("✅ Database Connected: careerflow_db");
+    console.log("✅ Database Connected to Aiven Cloud!");
     connection.release();
   }
 });
